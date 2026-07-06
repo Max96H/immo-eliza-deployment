4f6c95aedc71b584d4c90df4153cccd38ab83876
maxh <max.huberland@hotmail.fr>
2026-07-06 23:22:51 +0200

    Pydantic basemodel and POST /predict

--- api/api.py
+++ api/api.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    latitude: int
+    longitude: int
+    property_type: str
+    livable_surface: int | None = None
+    terrace: bool | None = 0
+    swimming_pool: bool | None = 0
+    garage: bool | None = 0
+    swimming_pool: bool | None = 0
+    swimming_pool: bool | None = 0
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "alive"
+
+@app.post("/predict")
+async def predict(item: Item):
+    return {"prediction": 0}
