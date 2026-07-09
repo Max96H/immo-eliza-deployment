84192e5089cc66fe8a6bea2c523b90a3f8ef1e65
maxh <max.huberland@hotmail.fr>
2026-07-09 11:59:46 +0200

    Postcode format and managing steps parameters

--- streamlit/stream.py
+++ streamlit/stream.py
@@ -22,7 +22,7 @@ st.markdown(f"Your selected option : {inputs["property_state"]}.")
 
 inputs["build_year"] = st.slider("Build year :", min_value=1500, max_value=2030, value=1985)
 
-inputs["postcode"] = st.number_input("Zipcode :", value=None, placeholder="1000")
+inputs["postcode"] = st.number_input("Zipcode :", value=None, placeholder="1000", format="%u", step=None)
 
 provinces = ['antwerp', 'limburg', 'east-flanders', 'vlaams-brabant', 
              'west-flanders', 'brussels', 'hainaut', 'liege', 'luxembourg',
@@ -33,12 +33,12 @@ inputs["bedroom_count"] = st.slider("Number of bedrooms :", 0, 20, 1)
 inputs["swimming_pool"] = st.toggle("Swimming pool")
 inputs["terrace"] = st.toggle("Terrace")
 inputs["garage"] = st.toggle("Garage")
-inputs["energy_consumption_kWh"] = st.number_input("Energy consumption (kWh/m2/year) :", value=150)
+inputs["energy_consumption_kWh"] = st.number_input("Energy consumption (kWh/m2/year) :", value=150, step=None)
 
 inputs["preschool_distance_m"] = st.slider("Preschool distance (m) :", 0, 10000, 350, 50)
 inputs["train_station_distance_m"] = st.slider("Train station distance (m) :", 0, 10000, 500, 50)
 inputs["supermarket_distance_m"] = st.slider("Supermarket distance (m) :", 0, 10000, 200, 50)
-inputs["nearest_city_distance_km"] = st.number_input("Nearest city distance (km) :", value=0.5)
+inputs["nearest_city_distance_km"] = st.number_input("Nearest city distance (km) :", value=0.5, step=0.5)
 
 if st.button("Predict"):
     if not (inputs["postcode"]):
